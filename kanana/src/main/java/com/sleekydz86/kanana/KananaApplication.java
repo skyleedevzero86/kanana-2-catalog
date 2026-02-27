f79d80946440d5ecfd2603ad6f978123dee24649
이경용 <sleekydz86@naver.com>
2026-02-27 13:24:56 +0900

    feat: 프로젝트 셋팅

---
+++ kanana/src/main/java/com/sleekydz86/kanana/KananaApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.kanana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KananaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KananaApplication.class, args);
+    }
+
+}
---
+++ kanana/src/test/java/com/sleekydz86/kanana/KananaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.kanana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KananaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
